1585881a642c951e2b13030f1196c8ff5749754b
mosama <mosama88@mail.com>
2025-06-01 14:14:14 +0300

    success

--- resources/views/dashboard/layouts/message.blade.php
+++ resources/views/dashboard/layouts/message.blade.php
@@ -1,8 +1,8 @@
 @if (session('success') || session('error'))
-    <div class="bs-toast toast toast-ex animate__animated my-2 fade bg-primary animate__tada hide {{ session('success') ? 'bg-success' : 'bg-danger' }}"
+    <div class="bs-toast toast toast-ex animate__animated my-2 fade {{ session('success') ? 'bg-success' : 'bg-danger' }} animate__tada hide "
         role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="5000">
         <div class="toast-header">
-            <i class="bx bx-bell me-2"></i>
+            <i class="{{ session('success') ? 'fa-solid fa-thumbs-up me-2' : 'fa-solid fa-triangle-exclamation' }}"></i>
             <div class="me-auto fw-semibold">{{ session('success') ? 'Success' : 'Error' }}</div>
             <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
         </div>
