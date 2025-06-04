76d62cf14333f679b2263eb3b61bea5416a1cbca
mosama88 <mosama88@hotmail>
2025-06-04 13:12:58 +0300

    Employees

--- database/migrations/2024_07_04_220445_create_employees_table.php
+++ database/migrations/2024_07_04_220445_create_employees_table.php
@@ -1,8 +1,20 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\City;
+use App\Models\Branch;
+use App\Models\Country;
+use App\Models\JobGrade;
+use App\Models\Language;
+use App\Models\BloodType;
+use App\Models\Department;
+use App\Models\ShiftsType;
+use App\Models\Governorate;
+use App\Models\JobCategory;
+use App\Models\Nationality;
+use App\Models\Qualification;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,90 +25,79 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
-            $table->integer('employee_code')->nullable()->comment('كود الموظف التلقائي لايتغير');
-            $table->integer('fp_code')->unique()->nullable()->comment('كود بصمة الموظف من جهاز البصمة لايتغير');
+            $table->integer('employee_code')->nullable(); //كود الموظف التلقائي لايتغير
+            $table->integer('fp_code')->unique()->nullable(); //كود بصمة الموظف من جهاز البصمة لايتغير
             $table->string('name', 300)->unique();
             $table->string('slug')->unique()->nullable();
-            $table->enum('gender', ['Male', 'Female'])->default('Male');
-            $table->foreignId('branch_id')->comment('الفرع التابع له الموظف ')->references('id')->on('branches')->onUpdate('cascade');
-            $table->foreignId('job_grade_id')->comment('الدرجه الوظيفية للموظف')->references('id')->on('job_grades')->onUpdate('cascade')->nullable();
-            $table->foreignId('qualification_id')->nullable()->comment('المؤهل التعليمي')->references('id')->on('qualifications')->onUpdate('cascade');
-            $table->string('qualification_year', 10)->nullable()->comment('سنة التخرج');
-            $table->string('major', 225)->nullable()->comment('تخصص التخرج');
-            $table->enum('graduation_estimate', ['Fair', 'Good', 'Very_Good', 'Excellent'])->nullable()->comment('تقدير التخرج ')->default('Fair');
-            $table->date('birth_date')->nullable()->comment('تاريخ ميلاد الموظف');
-            $table->string('national_id', 50)->unique()->nullable()->comment('رقم البطاقة الشخصية - او رقم الهوية');
-            $table->date('end_national_id')->nullable()->comment('تاريخ نهاية البطاقة الشخصية - بطاقة الهوية');
-            $table->string('national_id_place', 225)->nullable()->comment('مكان اصدار بطاقة الهوية الشخصية');
-            $table->foreignId('blood_types_id')->nullable()->comment('فصيلة الدم')->references('id')->on('blood_types')->onUpdate('cascade');
-            $table->enum('religion', ['Muslim', 'Christian'])->nullable()->comment('حقل الديانة')->default('Muslim');
-            $table->foreignId('language_id')->nullable()->references('id')->on('languages')->onUpdate('cascade')->comment(' اللغه التي يتكلم بها الاساسية');
-            $table->string('email', 100)->unique()->nullable()->comment(' ايميل  الموظف');
-            $table->foreignId('country_id')->nullable()->comment('دولة الموظف')->references('id')->on('countries')->onUpdate('cascade');
-            $table->foreignId('governorate_id')->nullable()->comment('محافظة الموظف')->references('id')->on('governorates')->onUpdate('cascade');
-            $table->foreignId('city_id')->nullable()->comment('مدينة الموظف')->references('id')->on('cities')->onUpdate('cascade');
-            $table->string('home_telephone', 50)->nullable()->comment('رقم هاتف المنزل');
-            $table->string('work_telephone', 50)->nullable()->comment('رقم هاتف العمل');
-            $table->string('mobile', 50)->nullable()->comment('رقم هاتف المحمول');
-            $table->enum('military', ['Exemption', 'Exemption_Temporary', 'Complete'])->nullable()->comment('الحالة العسكرية')->default('Exemption_Temporary');
-            $table->date('military_date_from')->nullable()->comment('تاريخ بداية الخدمة العسكرية');
-            $table->date('military_date_to')->nullable()->comment('تاريخ نهاية الخدمة العسكرية');
-            $table->string('military_wepon', 500)->nullable()->comment('نوع سلاح الخدمة العسكرية');
-            $table->date('military_exemption_date')->nullable()->comment('تاريخ الاعفاء من الخدمه العسكرية');
-            $table->string('military_exemption_reason', 500)->nullable()->comment('سبب الاعفاء من الخدمه العسكرية ');
-            $table->string('military_postponement_reason', 500)->nullable()->comment('سبب التأجيل من الخدمه العسكرية ');
-            $table->date('date_resignation')->nullable()->comment('تاريخ ترك العمل');
-            $table->string('resignation_reason')->nullable()->comment('سبب ترك العمل');
-            $table->enum('driving_license', ['Yes', 'No'])->nullable()->comment('هل يمتلك رخصه قياده')->default('No');
-            $table->enum('driving_license_type', ['Special', 'First', 'Second', 'Third', 'Fourth', 'Pro', 'Motorcycle'])->nullable()->comment('نوع رخصه القيادة')->default('Special');
-            $table->string('driving_License_id', 50)->nullable()->comment('رقم رخصه القيادة');
-            $table->enum('has_relatives', ['Yes', 'No'])->nullable()->default('No')->comment('هل له اقارب بالعمل ');
-            $table->string('relatives_details', 600)->nullable()->comment('تفاصيل الاقارب بالعمل');
+            $table->tinyInteger('gender')->default(1)->nullable();
+            $table->foreignIdFor(Branch::class)->nullable()->constrained()->nullOnDelete();
+            $table->foreignIdFor(JobGrade::class)->nullable()->constrained()->nullOnDelete(); //الدرجه الوظيفية للموظف
+            $table->foreignIdFor(Qualification::class)->nullable()->constrained()->nullOnDelete(); //المؤهل التعليمي للموظف
+            $table->year('qualification_year')->nullable(); //سنة التخرج
+            $table->string('major', 225)->nullable(); //تخصص التخرج
+            $table->tinyInteger('graduation_estimate')->default(1)->nullable(); //التقدير
+            $table->date('birth_date')->nullable(); //تاريخ ميلاد
+            $table->string('national_id', 50)->unique()->nullable(); //رقم البطاقة الشخصية
+            $table->date('end_national_id')->nullable(); //تاريخ نهاية البطاقة الشخصية
+            $table->string('national_id_place', 225)->nullable(); //مكان اصدار بطاقة الهوية الشخصية
+            $table->foreignIdFor(BloodType::class)->nullable()->constrained()->nullOnDelete(); //فصيلة الدم
+            $table->tinyInteger('religion')->default(1)->nullable(); //الديانة
+            $table->foreignIdFor(Language::class)->nullable()->constrained()->nullOnDelete(); //اللغه التي يتكلم بها الاساسية
+            $table->string('email', 100)->unique()->nullable(); //البريد الالكترونى
+            $table->foreignIdFor(Country::class)->nullable()->constrained()->nullOnDelete(); //دولة
+            $table->foreignIdFor(Governorate::class)->nullable()->constrained()->nullOnDelete(); //محافظة
+            $table->foreignIdFor(City::class)->nullable()->constrained()->nullOnDelete(); //مدينة
+            $table->string('home_telephone', 20)->nullable(); //رقم هاتف المنزل
+            $table->string('work_telephone', 20)->nullable(); //رقم هاتف العمل
+            $table->string('mobile', 20)->nullable(); //رقم هاتف المحمول
+            $table->string('address', 500)->nullable(); //عنوان الموظف
+            $table->tinyInteger('military')->default(1)->nullable(); //الحالة العسكرية
+            $table->date('military_service_start_date')->nullable(); //تاريخ بداية الخدمة العسكرية
+            $table->date('military_service_end_date')->nullable(); //تاريخ نهاية الخدمة العسكرية
+            $table->string('military_wepon', 200)->nullable(); //نوع سلاح الخدمة العسكرية
+            $table->date('military_exemption_date')->nullable(); //تاريخ الاعفاء من الخدمه العسكرية
+            $table->string('military_exemption_reason', 500)->nullable(); //سبب الاعفاء من الخدمه العسكرية
+            $table->string('military_postponement_reason', 500)->nullable(); //سبب التأجيل من الخدمه العسكرية
+            $table->date('date_resignation')->nullable(); //تاريخ ترك العمل
+            $table->string('resignation_reason')->nullable(); //سبب ترك العمل
+            $table->tinyInteger('driving_license')->default(1)->nullable(); //هل يمتلك رخصه قياده
+            $table->tinyInteger('driving_license_type')->default(1)->nullable(); //نوع رخصه القيادة
+            $table->string('driving_License_id', 20)->nullable(); //رقم رخصه القيادة
+            $table->tinyInteger('has_relatives')->default(1)->nullable(); //هل له اقارب بالعمل
+            $table->string('relatives_details', 600)->nullable(); //تفاصيل الاقارب بالعمل
             $table->text('notes')->nullable();
-            $table->date('work_start_date')->nullable()->comment('تاريخ بدء العمل للموظف');
-            $table->enum('functional_status', ['Employee', 'Unemployed'])->default('Employee')->comment('حالة الموظف');
-            $table->foreignId('department_id')->references('id')->on('departments')->onUpdate('cascade');
-            $table->foreignId('job_categories_id')->nullable()->references('id')->on('job_categories')->onUpdate('cascade');
-            $table->enum('has_attendance', ['Yes', 'No'])->nullable()->default('Yes')->comment('هل ملزم الموظف بعمل بصمه حضور وانصراف');
-            $table->enum('has_fixed_shift', ['Yes', 'No'])->nullable()->comment('هل للموظف شفت ثابت')->default('Yes');
-            $table->foreignId('shift_types_id')->nullable()->references('id')->on('shifts_types')->onUpdate('cascade');
-            $table->decimal('daily_work_hour', 20, 2)->nullable()->comment('عدد ساعات العمل للموظف وهذا في حالة ان ليس له شفت ثابت');
-            $table->decimal('salary', 20, 2)->nullable()->default(0)->comment('راتب الموظف');
-            $table->decimal('day_price', 10, 2)->nullable()->comment('سعر يوم الموظف');
-            $table->enum('motivation_type', ['Changeable', 'None', 'Fixed'])->nullable()->default('Fixed')->comment('صفر لايوجد - واحد ثابت - اثنين متغير');
-            $table->decimal('motivation', 20, 2)->nullable()->default(0)->comment('قيمة الحافز الثابت ان وجد');
-            $table->enum('social_insurance', ['Yes', 'No'])->nullable()->default('Yes')->comment('هل للموظف تأمين اجتماعي');
-            $table->decimal('social_insurance_cut_monthely', 20, 2)->nullable()->comment('  قيمة استقطاع التأمين الاجتماعي الشهري للموظف');
-            $table->string('social_insurance_number', 50)->nullable();
-            $table->enum('medical_insurance', ['Yes', 'No'])->nullable()->default('Yes')->comment('هل للموظف تأمين طبي');
-            $table->decimal('medical_insurance_cut_monthely', 20, 2)->nullable()->comment('  قيمة استقطاع التأمين الطبي الشهري للموظف');
-            $table->string('medical_insurance_number', 50)->nullable();
-            $table->enum('Type_salary_receipt', ['Cach', 'Visa'])->nullable()->default('Visa')->comment('نوع صرف الراتب - واحد كاش - اثنين فيزا بنكي');
-            $table->enum('active_vacation', ['Yes', 'No'])->nullable()->default('Yes')->comment('هل هذا الموظف ينزل له رصيد اجازات	');
-            $table->string('urgent_person_details', 600)->nullable()->comment('تفاصيل شخص يمكن الرجوع اليه للوصول للموظف');
-            $table->text('staies_address')->nullable()->comment('عنوان الاقامة الفعلي للموظف');
+            $table->date('hiring_date')->nullable(); //تاريخ التعيين
+            $table->tinyInteger('functional_status')->default(1)->nullable(); //حالة الموظف
+            $table->foreignIdFor(Department::class)->nullable()->constrained()->nullOnDelete(); //الادارة
+            $table->foreignIdFor(JobCategory::class)->nullable()->constrained()->nullOnDelete(); //نوع الوظيفه
+            $table->tinyInteger('has_attendance')->default(1)->nullable(); //هل ملزم الموظف بعمل بصمه حضور وانصراف
+            $table->tinyInteger('has_fixed_shift')->default(1)->nullable(); //هل للموظف شفت ثابت
+            $table->foreignIdFor(ShiftsType::class)->nullable()->constrained()->nullOnDelete(); //شفت
+            $table->decimal('daily_work_hour', 10, 2)->nullable(); //عدد ساعات العمل للموظف وهذا في حالة ان ليس له شفت ثابت
+            $table->decimal('salary', 10, 2)->nullable()->default(0); //راتب الموظف
+            $table->decimal('day_price', 10, 2)->nullable(); //سعر يوم الموظف
+            $table->string('bank_number_account', 50)->nullable(); //رقم حساب البنك للموظف
+            $table->tinyInteger('motivation_type')->default(1)->nullable(); //نوع الحافز
+            $table->decimal('motivation_value', 10, 2)->nullable()->default(0); //قيمة الحافز الثابت ان وجد
+            $table->tinyInteger('social_insurance')->default(1)->nullable(); //هل للموظف تأمين اجتماعي
+            $table->decimal('social_insurance_cut_monthely', 20, 2)->nullable(); // قيمة استقطاع التأمين الاجتماعي الشهري للموظف
+            $table->string('social_insurance_number', 50)->nullable(); //رقم التأمين الاجتماعي
+            $table->tinyInteger('medical_insurance')->default(1)->nullable(); //هل للموظف تأمين طبي
+            $table->decimal('medical_insurance_cut_monthely', 20, 2)->nullable(); //قيمة استقطاع التأمين الطبي الشهري للموظف
+            $table->string('medical_insurance_number', 50)->nullable(); //رقم التأمين الطبي
+            $table->tinyInteger('type_salary_receipt')->default(1)->nullable(); //نوع صرف الراتب
+            $table->tinyInteger('have_vacation_balance')->default(1)->nullable(); //هل هذا الموظف ينزل له رصيد اجازات	
+            $table->string('urgent_person_details', 600)->nullable(); //تفاصيل شخص يمكن الرجوع اليه للوصول للموظف
             $table->integer('children_number')->nullable()->default(0);
-            $table->enum('social_status', ['Divorced', 'Married', 'Single', 'Widowed'])->nullable()->default('Single')->comment('الحالة الاجتماعية');
-            $table->string('bank_number_account', 50)->nullable()->comment('رقم حساب البنك للموظف');
-            $table->enum('disabilities', ['Yes', 'No'])->nullable()->default('Yes')->comment('هل له اعاقة  - واحد يوجد صفر لايوجد');
-            $table->string('disabilities_type', 500)->nullable()->comment('نوع الاعاقة');
-            $table->foreignId('nationality_id')->nullable()->references('id')->on('nationalities')->onUpdate('cascade');
-            $table->string('name_sponsor')->nullable()->comment('اسم الكفيل ');
-            $table->string('pasport_identity', 100)->nullable()->comment('رقم الباسبور ان وجد');
-            $table->string('pasport_from_place', 100)->nullable()->comment('مكان استخراج الباسبور');
-            $table->date('pasport_exp_date')->nullable()->comment('تاريخ انتهاء الباسبور');
-
-            // Vacation
-            $table->string('num_vacation_days')->nullable()->comment('عدد ايام رصيد الأجازات');
-            $table->string('add_service')->nullable()->comment('أضافة خدمه مثل 3 سنوات تجنيد');
-            $table->string('years_service')->nullable()->comment('عدد سنوات الخدمه بالشركة');
-
-            $table->string('cv', 100)->nullable();
-            $table->string('basic_address_country', 300)->nullable()->comment('عنوان اقامة الموظف في بلده الام');
-            // $table->date("date");
-            $table->integer('fixed_allowances')->nullable()->default(0)->comment('هل له بدلات ثابته');
-            $table->tinyInteger('is_done_Vacation_formula')->nullable()->default(0)->comment('هل تمت المعادله التلقائية لاحتساب الرصيد السنوي للموظف');
-            $table->tinyInteger('is_Sensitive_manager_data')->nullable()->default(0)->comment('هل بيانات حساساه للمديرين مثلا ولاتظهر الا بصلاحيات خاصة	');
+            $table->tinyInteger('social_status')->default(1)->nullable(); //الحالة الاجتماعية
+            $table->tinyInteger('disabilities')->default(1)->nullable(); //هل له اعاقة
+            $table->string('disabilities_type', 500)->nullable(); //نوع الاعاقة
+            $table->foreignIdFor(Nationality::class)->nullable()->constrained()->nullOnDelete(); //الجنسية
+            $table->string('pasport_identity', 100)->nullable(); //رقم الباسبور ان وجد
+            $table->date('pasport_exp_date')->nullable(); //تاريخ انتهاء الباسبور
+            $table->tinyInteger('fixed_allowances')->nullable()->default(2); //هل له بدلات ثابته
+            $table->tinyInteger('is_done_Vacation_formula')->nullable()->default(1); //هل تمت المعادله التلقائية لاحتساب الرصيد السنوي للموظف
+            $table->tinyInteger('is_Sensitive_manager_data')->nullable()->default(1); //هل بيانات حساساه للمديرين مثلا ولاتظهر الا بصلاحيات خاصة
             $table->foreignId('created_by')->references('id')->on('admins')->onUpdate('cascade');
             $table->foreignId('updated_by')->nullable()->references('id')->on('admins')->onUpdate('cascade');
             $table->integer('com_code');
